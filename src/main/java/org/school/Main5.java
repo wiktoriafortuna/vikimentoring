b796428b17dda464f107504d519c54afe59597c6
bartlomiejkalka <bartlomiejkalqa@gmail.com>
2025-01-13 17:56:29 +0100

    after exercises

--- src/main/java/org/school/Main5.java
+++ src/main/java/org/school/Main5.java
@@ -5,18 +5,21 @@ import java.util.List;
 
 public class Main5 {
     public static void main(String[] args) {
-        Student student1= new Student ( "Andrzej");
-        Student student2= new Student ("Michael");
-        List<Student> studentsOldSchool= new ArrayList<>();
-        List<Student> studentsNewSchool = new ArrayList<>();
-        studentsOldSchool.add(student1);
-        studentsNewSchool.add(student2);
+        School school1 = new School( "SchoolkNr1");
+        school1.addStudent("Andrzej");
+        school1.addStudent("ANNA");
+        school1.addStudent("Mieszko");
+        school1.addStudent("Paweł");
 
-        School school1 = new School(studentsOldSchool,"SchoolkNr1");
-        School school2 = new School(studentsNewSchool,"SchoolkNr2");
-        boolean isStudentAvailable = school1.isEnrolled("Andrzej");
+        School school2 = new School( "SchoolkNr2");
+        school2.addStudent("Michael");
+        school2.addStudent("Wiktoria");
+
+        boolean isStudentAvailable = school1.isEnrolled("andrzej");
         boolean isStudentAvailable2 = school2.isEnrolled("Michael");
+        boolean isStudentAvailable3 = school1.isEnrolled("andrZeJ");
         System.out.println(isStudentAvailable);
         System.out.println(isStudentAvailable2);
+        System.out.println(isStudentAvailable3);
     }
 }
--- src/main/java/org/school/School.java
+++ src/main/java/org/school/School.java
@@ -1,23 +1,33 @@
 package org.school;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class School {
    public List<Student> students;
    public String name;
 
-    School(List<Student>students, String name) {
-        this.students = students;
+    School(String name) {
+        this.students = new ArrayList<>();
         this.name = name;
     }
 
     public boolean isEnrolled(String studentName){
-        for(Student student : students){
-            if(student.getName().equals(studentName)){
+        for(Student student : this.students){
+            String name1 = student.getName();
+//            String andrzejToUpperCase = name1.toUpperCase();
+//            String andrzejToUpperCase2 = studentName.toUpperCase();
+            if(name1.equalsIgnoreCase(studentName)){
+                // sprawdzaj czy AndRZej to też andRZej
                 return true;
             }
         }
         return false;
+    }
+
+    public void addStudent(String studentName){
+        Student student = new Student(studentName);
+        this.students.add(student);
     }
 
 }
